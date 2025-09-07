a85eabb10ed918699d7e0d3dd3a9955deb50b19f
omarfarukcodes69 <omarfaruk.codes69@gmail.com>
2025-09-08 00:13:36 +0600

    upadate changes

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 // all container call
 const categorContainer = document.getElementById("category-container");
 const cardsContainers = document.getElementById("cards-container");
+const yourCardsContainers = document.getElementById("add-container");
 
 // ..... all api data load function .......
 const loadAllPlants = () => {
@@ -24,12 +25,32 @@ const loadCardApiByCetagoryId = (cetagoryID) => {
         .then(cardsDetails => showcardByCategory(cardsDetails.
             plants))
 }
-
 //  -------- cal add to card button ---------
-const addCartBtn = ()=>{
-    console.log(" add to card btn clicked");
-    console.log(cardsContainers)
-}
+cardsContainers.addEventListener("click", (e) => {
+    if (e.target.localName === 'button') {
+        console.log(e.target)
+        const totalPrice = document.getElementById("total-price").innerText;
+        const plantsName = e.target.parentNode.children[0].innerText;
+        const plantsPrice = e.target.parentNode.children[2].children[1].children[1].innerText;
+        const currenPrice = Number(totalPrice) + Number(plantsPrice);
+        document.getElementById("total-price").innerText = currenPrice;
+        console.log()
+
+        yourCardsContainers.innerHTML += `
+                    <div  class=" bg-[#F0FDF4] px-4 py-2 rounded-md flex justify-between items-center my-3 ">
+                        <div class="title space-y-2">
+                        <h1 class="text-xl font-bold">${plantsName}</h1>
+                        <p class="text-md text-gray-400 "><span>৳</span><span> ${plantsPrice}</span></p>
+                    </div>
+                    <div class=" cursor-pointer"><i class="fa-solid fa-xmark"></i></div>
+                </div>
+
+        `
+        // console.log(plantsName, plantsPrice)
+
+    }
+
+})
 // ........ display or showing data into UI ....
 const showingCatagory = (categories) => {
     categories.forEach(category => {
@@ -38,6 +59,7 @@ const showingCatagory = (categories) => {
             <li id="${category.id}" class="hover:bg-green-500 py-2 px-3 rounded-md btn-category cursor-pointer">${category.category_name}</li>
   `
     });
+    // ----- Category btn feature -------
     categorContainer.addEventListener("click", (e) => {
         const caterogyList = document.querySelectorAll('li');
         caterogyList.forEach(li => {
@@ -50,6 +72,7 @@ const showingCatagory = (categories) => {
     })
 
 }
+
 const showcardByCategory = (plants) => {
     cardsContainers.innerHTML = " ";
     plants.forEach(plant => {
@@ -64,9 +87,9 @@ const showcardByCategory = (plants) => {
                             <p>${plant.description} </p>
                             <div class=" text-lg flex items-center justify-between">
                                 <p class="text-center p-1 bg-[#DCFCE7] text-[#166534] rounded-3xl ">${plant.category}</p>
-                                <p class=" font-bold text-right">৳${plant.price}</p>
+                                <p class=" font-bold text-right"><span>৳</span><span>${plant.price}</span></p>
                             </div>
-                            <button onclick="addCartBtn()" class="btn w-full py-6 bg-[#166534] text-white text-lg rounded-3xl">Add to
+                            <button class="btn w-full py-6 bg-[#166534] text-white text-lg rounded-3xl">Add to
                                 Cart</button>
                         </div>
                     </div>
